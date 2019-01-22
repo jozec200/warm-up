40000e11df25f5349177471cb751093d1f773bd6
Jozec <jozec.200@protonmail.com>
2019-01-21 23:46:16 -0600

    Uploading basic exercises

---
+++ basics/ex-07-getMinimum.js
@@ -0,0 +1,31 @@
+/**
+ * getMinimum()
+ * ------------------------------------------------------
+ * Write a function called `getMinimum()` that takes 4 numbers as arguments
+ * and returns the one with the lowest value.
+ *
+ **/
+
+// ++ YOUR Code below (write the 'makeGreeting' function): ++
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
+/*-------------------TEST-1-------------------------*/
+// function receives 4 number inputs
+//    and returns the minimum value
+/*--------------------------------------------------*/
+console.log("==== ex-07-getMinimum : TEST 1 ====");
+
+
+console.assert( getMinimum(1,2,4,0) === 0 )
+console.assert( getMinimum(1,2,4,0) !== 1 )
+
+console.assert( getMinimum(4,8,4,20) === 4 )
+
+console.assert( getMinimum(300,-2,-99,0) === -99 )
+
+console.assert( getMinimum(-5,-11,4,-5) === -11 )
